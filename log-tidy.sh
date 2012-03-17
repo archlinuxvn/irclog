a8e48eb0b7f07e0bfd00902d3edbd20f15fe08eb
icy <kyanh@viettug.org>
2012-03-17 18:56:04 +0700

    tidylog will skip existing files

--- log-tidy.sh
+++ log-tidy.sh
@@ -7,13 +7,19 @@
 # Date   : 2012 March 17th
 
 for f in ./archives/*.txt; do
-  echo ":: Processing $f... -> ./www/irclogs/"
   _month="$(basename $f | awk -F '-' '{printf("%s/%s\n", $1, $2)}')"
   _d_month="./www/irclogs/$_month/"
   _f_output="$_d_month/$(basename $f)"
-  mkdir -pv "$_d_month/"
-  cat $f \
-    | sed -e 's#[.0-9]\+\]#1.2.3.4]#g' \
-    | fold -s -w 150 \
-    > $_f_output
+
+  if [[ ! -f $_f_output || $_f_output -ot $f ]]; then
+    echo ":: Processing $f... -> ./www/irclogs/"
+    mkdir -pv "$_d_month/"
+    cat $f \
+      | sed -e 's#[.0-9]\+\]#1.2.3.4]#g' \
+      | fold -s -w 150 \
+      > $_f_output
+    touch -r $f $_f_output
+  else
+    echo ":: File $_f_output is newer than the source $f"
+  fi
 done
