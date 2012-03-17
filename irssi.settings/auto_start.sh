61def88fc4a11627dd1d1728a2937347374b0652
icy <kyanh@viettug.org>
2012-03-17 22:42:14 +0700

    Add simple script to start irssi at boot time

---
+++ irssi.settings/auto_start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Purpose: Simple script to start irssi at bootime
+# Link:    http://dragula.org/blogs/672
+# Date:    2011 March 17th
+# Author:  Anh K. Huynh
+# License: Fair license
+
+tmux new-session -d -s 0
+tmux new-window -t 0:1 -n 'irssi' 'irssi'
+tmux select-window -t 0:1
+tmux detach-client
+
