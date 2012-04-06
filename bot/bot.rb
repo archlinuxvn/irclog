8025755df2f6b0049c558498d400a3b1406ede32
icy <kyanh@viettug.org>
2012-04-06 11:16:16 +0700

    bot: add new function info, help, tinyurl. Fix sensored words list

--- bot/bot.rb
+++ bot/bot.rb
@@ -65,7 +65,7 @@ bot = Cinch::Bot.new do
   end
 
   # Sensored words
-  on :message, /(vcl|wtf|sh[1i]t|f.ck|d?k)/i do |m, text|
+  on :message, /(vcl|wtf|sh[1i]t|f.ck|d[e3]k|clgt)/i do |m, text|
     m.reply "#{m.user.nick}: take it easy. don't say #{text}"
   end
 
@@ -79,9 +79,27 @@ bot = Cinch::Bot.new do
     end
   end
 
-  on :channel do |m|
-    urls = URI.extract(m.message, "http")
+  on :message, /^archl0n0xvn: info/ do |m|
+    m.reply "ArchLinuxVn aka Vietnamese Groups of ArchLinux Users"
+    m.reply " * irc channel  #archlinuxvn on irc.freenode.net"
+    m.reply " * homepage     http://archlinuxvn.tuxfamily.org/"
+    m.reply " * mailing list http://groups.google.com/group/archlinuxvn"
+    m.reply " * source code  http://github.com/archlinuxvn/"
+  end
 
+  on :message, /^archl0n0xvn: help/ do |m|
+    m.reply "Available commands: info, help, tinyurl"
+    m.reply "The bot will say hello sometimes"
+    m.reply "To fix the bot's behavior, visit http://github.com/archlinuxvn/irclog"
+  end
+
+  # Provide a simple command , example
+  # botname: tinyrul <your_url>. The bot will reply to the author
+  # a tiny version of your URL. HTTP and HTTPS only.
+  on :message, /^archl0n0xvn: tinyurl (https?:\/\/[^ ]+)/ do |m, url|
+    # urls = URI.extract(m.message, "http")
+    # puts ":::: #{urls}"
+    urls = [] << url.strip
     unless urls.empty?
       short_urls = urls.map {|url| shorten(url) }.compact
 
