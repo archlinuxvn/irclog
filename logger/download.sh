204f5b1433d1fb317ebfca2918acd4b5fa865710
icy <kyanh@viettug.org>
2012-04-14 07:11:18 +0700

    logger: Support mirrors

---
+++ logger/download.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Purpose: Download irssi logs from remotes
+# Author : Anh K. Huynh (kyanh@viettug.org)
+# Date   : 2012 Apr 14th
+# License: Fair license
+
+cd archives \
+&& {
+  for f in archlinuxvn m1.archlinuxvn; do
+    [[ -f $f.log.gz ]] && gunzip $f.log.gz
+    case $f in
+      'archlinuxvn')     _R_="irc.archlinuxvn.m0" ;;
+      'm1.archlinuxvn')  _R_="irc.archlinuxvn.m1" ;;
+    esac
+    echo ":: Info: Transferring file from $_R_"
+    rsync -avessh --progress "${_R_}:~/irclogs/freenode/\#archlinuxvn.log" $f.log
+    gzip $f.log
+  done
+}
--- logger/log-tidy.sh
+++ logger/log-tidy.sh
@@ -6,15 +6,15 @@
 # License: Fair license
 # Date   : 2012 March 17th
 
-for f in ./archives/*.txt; do
+for f in ./archives/*.txt.gz; do
   _month="$(basename $f | awk -F '-' '{printf("%s/%s\n", $1, $2)}')"
   _d_month="./www/irclogs/$_month/"
-  _f_output="$_d_month/$(basename $f)"
+  _f_output="$_d_month/$(basename $f .gz)"
 
   if [[ ! -f $_f_output || $_f_output -ot $f ]]; then
     echo ":: Processing $f... -> ./www/irclogs/"
     mkdir -pv "$_d_month/"
-    cat $f \
+    zcat $f \
       | sed -e 's#[.0-9]\+[0-9]\]#1.2.3.4]#g' \
       | fold -s -w 150 \
       | cat -n \
