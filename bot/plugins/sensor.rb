de348326b2f2b52a401f7f72f66c51c81a2bd361
icy <kyanh@viettug.org>
2012-04-27 15:03:58 +0700

    bot: fix typo

--- bot/plugins/sensor.rb
+++ bot/plugins/sensor.rb
@@ -7,7 +7,7 @@
 class Sensor
   include Cinch::Plugin
 
-  set :help => "Send warning if user says bad words. After the warning is sent, the bot will record the event's time and it will not report again within #{BOT_CACHE_TIME} seconds. So... you are safe to say anything you want in #{BOT_CACHE_TIME} seconds without bothering the bot :D. BUG: if your nick match the pattern '#{BOT_NAME}', the bot will simply ignore anything you say. This is a KNOWN bug, so it is't funny if you are trying to trick the bot this way :)"
+  set :help => "Send warning if user says bad words. After the warning is sent, the bot will record the event's time and it will not report again within #{BOT_CACHE_TIME} seconds. So... you are safe to say anything you want in #{BOT_CACHE_TIME} seconds without bothering the bot :D. BUG: if your nick matches the pattern '#{BOT_NAME}', the bot will simply ignore anything you say. This is a KNOWN bug, so it isn't funny if you are trying to trick the bot this way :)"
 
   listen_to :message
 
