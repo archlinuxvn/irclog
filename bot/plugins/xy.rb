f76127c6c4422c096d7345c641edac7117cbfebe
icy <kyanh@viettug.org>
2013-03-10 17:21:37 +0700

    bot.give: allow user to bet
    the logic may conflict. you are not allowed to give a bet number
    that is greater than their current number of nutshells. however,
    when the do not any nutshell, they can play without a bet number

--- bot/plugins/xy.rb
+++ bot/plugins/xy.rb
@@ -7,16 +7,26 @@
 class Xy
   include Cinch::Plugin
 
-  set :help => "Play XY-game with the bot. To start the game, type `!xy <flag>`, where `<flag>` is `bao`, `keo`, `bua`. The bot will randomly choose its flag and find the winner. The rule is very simple: `bua > keo > bao > bua`. The winner will get a random number of nutshells (5, 10 or 15). The l00s3r will have to transfer a random number of nutshell to the masterbank (5, 10, 15 nutshells). See `!nutshell :masterbank`."
+  set :help => "Play XY-game with the bot. To start the game, type `!xy <flag> [<bet>]`, where `<flag>` is `bao`, `keo`, `bua`. The bot will randomly choose its flag and find the winner. The rule is very simple: `bua > keo > bao > bua`. The winner will get/lost a random number of nutshells (5, 10 or 15). You can also give a bet number, for example `!xy bua 50`. The bet number should not excess 100 or your currrent number of nutshells."
 
-  match /xy (.+)/,  :method => :xy_play
+  match /xy ([^[:space:]]+)([[:space:]]+[0-9]+)?\b/,  :method => :xy_play
 
-  def xy_play(m, flag)
-    return unless _cache_expired?(:xy, "play by #{m.user.nick}", :cache_time => 30)
+  def xy_play(m, flag, mscore)
+    # return unless _cache_expired?(:xy, "#{m.user.nick}", :cache_time => 30)
 
     flag = flag.strip.downcase
     flag = "bua" if flag.match(/^b.a$/)
     flag = "keo" if flag.match(/^k.o$/)
+    flag = "bao" if flag.match(/^b.o$/)
+
+    if mscore
+      mscore = mscore.strip.to_i.abs
+      if mscore > 100 or mscore > bot_score!(m.user.nick,0)
+        m.reply "#{m.user.nick}: You can't bet more than 100 or more than you have."
+        return
+      end
+    end
+
     bot_flag   = %w{bua bao keo}[rand(3)]
     user_index = %w{bua keo bao bua}.index(flag)
     bot_index  = %w{bua keo bao bua}.index(bot_flag)
@@ -25,28 +35,29 @@ class Xy
       if user_index
         case (user_index - bot_index).abs
           when 0,3 then Xy::_win?(0)
-          when 1   then Xy::_win?(bot_index - user_index)
-          when 2   then Xy::_win?(user_index - bot_index)
+          when 1   then Xy::_win?(bot_index - user_index, mscore)
+          when 2   then Xy::_win?(user_index - bot_index, mscore)
           else     [10, "Oops, the bot is buggy"]
         end
       else
-        [-1, "Do you try to trick the bot?"]
+        [-1, "Wanna a trick?"]
       end
 
     bot_nutshell_give!(:masterbank, m.user.nick, nutshell, :allow_doubt => true, :reason => "xy_play")
-    new_score = bot_score!(m.user.nick, 0)
-    m.reply "#{m.user.nick}: #{ret}. Got #{nutshell}. Now have #{new_score} nutshell(s)"
+    m.reply "#{m.user.nick}: #{ret}. Got #{nutshell}. Now have #{bot_score!(m.user.nick, 0)} nutshell(s)"
   end
 
   class << self
     # Return [score, message]
     # Score > 0: Win, get a positive number of nutshells (up to 3)
     # Score < 0: Loose, get a negative number of nutshells (up to 2)
-    def _win?(score)
+    # Mscore: nil or the amount that user bet
+    def _win?(score, mscore = nil)
+      mscore = 5 * (1+rand(3)) if not mscore or mscore == 0
       if score > 0
-        [5 * (1 + rand(3)), "You win"]
+        [mscore, "You win"]
       elsif score < 0
-        [5 * (-1 - rand(3)), "You loose"]
+        [- mscore, "You loose"]
       else
         [0, "Draw"]
       end
