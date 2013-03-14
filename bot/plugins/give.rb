bf7c93fcd4f85f3aa5f1eacbaea3f34937f87ca8
icy <kyanh@viettug.org>
2013-03-14 19:12:54 +0700

    give.tinyurl: fix bug
    when user give tinyurl to someone the ruby code of the
    real invocation is sent to them

--- bot/plugins/give.rb
+++ bot/plugins/give.rb
@@ -20,7 +20,7 @@ class Give
       wiki = args.gsub(" ", "%20")
       wiki ? "#{someone}: https://wiki.archlinux.org/index.php/Special:Search/#{wiki}" : nil
     when "tinyurl" then
-      "#{someone}: tinyurl(args)"
+      "#{someone}: " + tinyurl(args).to_s
     when "some"
       case args
         when "thanks" then "#{someone}: Thank you very much"
